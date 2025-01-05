9b9f02c8c16980f410010b953df19578f4650108
Alex Constable <alexander.constable@icloud.com>
2025-01-05 15:43:31 +0000

    Version 1 of Prime Number Detection with a Random Number Generator.

---
+++ prime_number_detector.py
@@ -0,0 +1,27 @@
+import random
+
+def isprime(austen):
+
+    isprime_flag = True
+    for c in range(2, austen):
+        d = austen / c
+        if d.is_integer():
+            isprime_flag = False
+    return isprime_flag
+
+print("Enter A Number Or Hit Enter To Generate A Random Number: ")
+
+captured_input = input()
+
+if captured_input == "":
+    y = random.randrange(1, 100) + 1
+    print(f"RANDOM NUMBER: {y}")
+else:
+    y = int(captured_input)
+
+for b in range(1, y):
+    if isprime(b):
+        print(f"PRIME NUMBER: {b}")
+       
+    
+
