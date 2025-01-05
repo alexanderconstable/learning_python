447310440d4926e121518cdfc7508d600fde60e0
Alex Constable <alexander.constable@icloud.com>
2025-01-05 16:11:11 +0000

    Added function main() to repeat the code until q is entered.

--- prime_number_detector.py
+++ prime_number_detector.py
@@ -1,7 +1,7 @@
 import random
 
-def isprime(number):
 
+def isprime(number):
     isprime_flag = True
     for c in range(2, number):
         d = number / c
@@ -9,19 +9,24 @@ def isprime(number):
             isprime_flag = False
     return isprime_flag
 
-print("Enter A Number Or Hit Enter To Generate A Random Number: ")
 
-captured_input = input()
+def main():
+    print("Enter A Number Or Hit Enter To Generate A Random Number: ")
 
-if captured_input == "":
-    y = random.randrange(1, 100) + 1
-    print(f"RANDOM NUMBER: {y}")
-else:
-    y = int(captured_input)
+    captured_input = input()
 
-for b in range(1, y):
-    if isprime(b):
-        print(f"PRIME NUMBER: {b}")
-       
-    
+    if captured_input == "q":
+        exit()
+    elif captured_input == "":
+        y = random.randrange(1, 100) + 1
+        print(f"RANDOM NUMBER: {y}")
+    else:
+        y = int(captured_input)
 
+    for b in range(1, y):
+        if isprime(b):
+            print(f"PRIME NUMBER: {b}")
+
+
+while True:
+    main()
